71f8119b7ad2dcef472903215ca004a1a2cf3c8b
Paul Dornfeld <pdornfel@gmail.com>
2014-02-19 11:31:20 -0500

    fixed images

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 VertextTemplate::Application.routes.draw do
+
+  root "welcomes#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
